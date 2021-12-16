16f28ed4dd220c8e3411866bf7bd7b3e2fb776e0
Dmitry Balikhin <dmitry.balikhin@assentcompliance.com>
2021-12-16 16:06:05 -0500

    initial push

---
+++ src/ApsantaScanner/DoNotUseInsecureRandomness.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the MIT license.  See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Analyzer.Utilities;
+using Analyzer.Utilities.Extensions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Apsanta.Scanner
+{
+ 
+    [DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic)]
+    public sealed class DoNotUseInsecureRandomness : DiagnosticAnalyzer
+    {
+        internal const string DiagnosticId = "CA5394";
+
+        internal static readonly DiagnosticDescriptor Rule = DiagnosticDescriptorHelper.Create(
+            DiagnosticId,
+            "DoNotUseInsecureRandomness",
+            "DoNotUseInsecureRandomness",
+            DiagnosticCategory.Security,
+            RuleLevel.Disabled,
+            description: "DoNotUseInsecureRandomness",
+            isPortedFxCopRule: false,
+            isDataflowRule: false);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.EnableConcurrentExecution();
+
+            // Security analyzer - analyze and report diagnostics on generated code.
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+
+            context.RegisterCompilationStartAction(compilationStartAnalysisContext =>
+            {
+                if (!compilationStartAnalysisContext.Compilation.TryGetOrCreateTypeByMetadataName(WellKnownTypeNames.SystemRandom, out var randomTypeSymbol))
+                {
+                    return;
+                }
+
+                compilationStartAnalysisContext.RegisterOperationAction(operationAnalysisContext =>
+                {
+                    var invocationOperation = (IInvocationOperation)operationAnalysisContext.Operation;
+                    var typeSymbol = invocationOperation.TargetMethod.ContainingType;
+
+                    if (randomTypeSymbol.Equals(typeSymbol))
+                    {
+                        operationAnalysisContext.ReportDiagnostic(
+                            invocationOperation.CreateDiagnostic(
+                                Rule,
+                                typeSymbol.Name));
+                    }
+                }, OperationKind.Invocation);
+            });
+        }
+    }
+}
