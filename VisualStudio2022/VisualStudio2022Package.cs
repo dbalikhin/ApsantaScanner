8856f24ba4805316939b6f0023a738da380fe737
Dmitry Balikhin <dmodeus@gmail.com>
2022-01-06 16:26:55 -0500

    compilation and workspace initial

--- VisualStudio2022/VisualStudio2022Package.cs
+++ VisualStudio2022/VisualStudio2022Package.cs
@@ -2,23 +2,40 @@
 global using Microsoft.VisualStudio.Shell;
 global using System;
 global using Task = System.Threading.Tasks.Task;
+using Microsoft.CodeAnalysis;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Linq;
 using VisualStudio2022.MarkdownViewer.Margin;
 
 namespace VisualStudio2022
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-    [ProvideToolWindow(typeof(MyToolWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
+    [ProvideToolWindow(typeof(MyToolWindow.Pane), Style = VsDockStyle.Float, Window = WindowGuids.MainWindow)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.VisualStudio2022String)]
     public sealed class VisualStudio2022Package : ToolkitPackage
     {
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
+            var componentModel = (IComponentModel)Package.GetGlobalService(typeof(SComponentModel));
+            var workspace = (Workspace)componentModel.GetService<VisualStudioWorkspace>();
+            foreach (var project in workspace.CurrentSolution.Projects)
+            {
+                
+                var compilation = await project.GetCompilationAsync();
+
+                var diagnostics = compilation?.GetDiagnostics().Where(d => d.Severity != DiagnosticSeverity.Hidden);
+                  
+                
+            }
             await this.RegisterCommandsAsync();
-            var mb = new MarkdownBrowser();
+            
+
+
             this.RegisterToolWindows();
         }
     }
